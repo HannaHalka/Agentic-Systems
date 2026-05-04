24e4ce37d2a0f8fa31e405584d62d26076a8aea7
Maryna Kosse <mkosse@kse.org.ua>
2026-05-04 17:08:48 +0300

    MCP bug fixes

--- mcp_server.py
+++ mcp_server.py
@@ -1,46 +1,46 @@
 from fastmcp import FastMCP
 from github_api import GitHubAPI
 
-mcp = FastMCP("Questions Management Server")
+mcp = FastMCP("My MCP")
 
 
-# RESPONSE DATA FORMAT UNCHECKED
-@mcp.resource("issues://{issue_number}")
-def get_issue_resource(owner, repo, issue_number) -> str:
+@mcp.resource("resource://{owner}/{repo}/{issue_number}")
+def get_issue_resource(owner, repo, issue_number):
     """Get a specific issue from the specified repository as a resource"""
     try:
         issue = GitHubAPI().get_issue(owner=owner, repo=repo, issue_number=issue_number)
 
-        if not issue.data:
+        if not issue["data"]:
             return "No issue found."
 
-        return issue.data
+        return issue["data"]
     except Exception as e:
         return f"Error retrieving issues: {str(e)}"
 
 
 @mcp.prompt()
-def list_all_issues(owner, repo) -> str:
+def list_all_issues(owner, repo):
     """Generate a formatted list all issues from the specified repository"""
     try:
         issues = GitHubAPI().list_repository_issues(owner=owner, repo=repo, state=None)
 
-        if not issues.data:
+        if not issues["data"]:
             return "No issues found for the repository."
 
         formatted_issues = []
-        for i, issue in enumerate(issues.data, 1):
+        for i, issue in enumerate(issues["data"], 1):
             formatted_issues.append(
-                f"{i}. ID: {issue.id}\n"
-                f"    Author: {issue.author_id}\n"
-                f"    Body: {issue.body}\n"
+                f"{i}. ID: {issue['id']}\n"
+                f"    Title: {issue['title']}\n"
+                f"    Body: {issue['body']}\n"
             )
 
-        result = f"Found {len(issues.data)} issues:\n\n" + "\n".join(formatted_issues)
+        result = f"Found {len(issues['data'])} issues: \n\n" + "\n".join(formatted_issues)
 
         return result
     except Exception as e:
         return f"Error retrieving issues: {str(e)}"
 
 
-mcp.run()
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
